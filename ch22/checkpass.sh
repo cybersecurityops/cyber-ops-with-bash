573d6765eab08443332e222b44c660cd47248372
Digadel <sales@digadel.com>
2019-10-12 14:14:56 -0400

    Relocated echo statement to improve output format.

--- ch22/checkpass.sh
+++ ch22/checkpass.sh
@@ -16,16 +16,16 @@ if (( "$#" == 0 ))                              #<1>
 then
     printf 'Enter your password: '
     read -s passin                              #<2>
+	echo
 else
-    passin="$1"
-    echo
+    passin="$1"    
 fi
 
 passin=$(echo -n "$passin" | sha1sum)	          #<3>
 passin=${passin:0:40}
 
 firstFive=${passin:0:5}                         #<4>
-ending=${passin:6}
+ending=${passin:5}
 
 pwned=$(curl -s "https://api.pwnedpasswords.com/range/$firstFive" | \
         tr -d '\r' | grep -i "$ending" )        #<5>
